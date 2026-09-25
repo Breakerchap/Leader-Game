bf9a464397953d586e5fe5561a55c694158bd0f3
Remy E <remyscalendar@gmail.com>
2026-09-25 16:09:43 +1000

    Add political power-base types

---
+++ Simulation/Politics/PowerBaseType.cs
@@ -0,0 +1,15 @@
+namespace LeaderGame.Simulation.Politics;
+
+public enum PowerBaseType
+{
+    Aristocracy,
+    Military,
+    Merchants,
+    Clergy,
+    Bureaucracy,
+    Workers,
+    Peasantry,
+    RegionalElites,
+    Party,
+    RoyalFamily
+}
