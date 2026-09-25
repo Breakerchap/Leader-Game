654a2d29c20c5ef21017f601fd14db78610bf67d
Remy E <remyscalendar@gmail.com>
2026-09-25 16:21:45 +1000

    Let material conditions shift political backing

--- Simulation/Systems/PoliticalSystem.cs
+++ Simulation/Systems/PoliticalSystem.cs
@@ -1,3 +1,4 @@
+using LeaderGame.Simulation.Countries;
 using LeaderGame.Simulation.Politics;
 
 namespace LeaderGame.Simulation.Systems;
@@ -21,6 +22,9 @@ internal static class PoliticalSystem
 
             if (country.Ruler.IsAlive)
             {
+                if (state.Date.Month % 3 == 0)
+                    ApplyConditionDrivenBacking(country);
+
                 var rulerBacking =
                     PoliticalCalculations.GetPowerBaseInfluence(country, country.Ruler);
 
@@ -43,6 +47,80 @@ internal static class PoliticalSystem
                 relationship.Trust = DriftTowards(relationship.Trust, 50, 1);
             }
         }
+    }
+
+    private static void ApplyConditionDrivenBacking(Country country)
+    {
+        var ruler = country.Ruler;
+
+        ApplyFundingReaction(
+            ruler,
+            PowerBaseType.Military,
+            country.ArmyFunding);
+
+        ApplyFundingReaction(
+            ruler,
+            PowerBaseType.Bureaucracy,
+            country.AdministrationFunding);
+
+        var courtReaction = FundingReaction(country.CourtFunding);
+        ruler.ChangePowerBaseStanding(PowerBaseType.Aristocracy, courtReaction);
+        ruler.ChangePowerBaseStanding(PowerBaseType.Clergy, courtReaction);
+        ruler.ChangePowerBaseStanding(PowerBaseType.RegionalElites, courtReaction);
+        ruler.ChangePowerBaseStanding(PowerBaseType.RoyalFamily, courtReaction);
+
+        if (country.TaxRate >= 0.16m)
+        {
+            ruler.ChangePowerBaseStanding(PowerBaseType.Merchants, -2);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Workers, -2);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Peasantry, -2);
+        }
+        else if (country.TaxRate <= 0.07m)
+        {
+            ruler.ChangePowerBaseStanding(PowerBaseType.Merchants, 1);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Workers, 1);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Peasantry, 1);
+        }
+
+        if (country.WarExhaustion >= 50)
+        {
+            ruler.ChangePowerBaseStanding(PowerBaseType.Military, -2);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Workers, -1);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Peasantry, -1);
+        }
+
+        if (country.PublicUnrest >= 60)
+        {
+            ruler.ChangePowerBaseStanding(PowerBaseType.Merchants, -1);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Bureaucracy, -1);
+            ruler.ChangePowerBaseStanding(PowerBaseType.Party, -2);
+            ruler.ChangePowerBaseStanding(PowerBaseType.RegionalElites, -1);
+        }
+    }
+
+    private static void ApplyFundingReaction(
+        Characters.Character ruler,
+        PowerBaseType powerBase,
+        decimal funding)
+    {
+        ruler.ChangePowerBaseStanding(powerBase, FundingReaction(funding));
+    }
+
+    private static int FundingReaction(decimal funding)
+    {
+        if (funding <= 0.75m)
+            return -2;
+
+        if (funding < 0.95m)
+            return -1;
+
+        if (funding >= 1.25m)
+            return 2;
+
+        if (funding > 1.05m)
+            return 1;
+
+        return 0;
     }
 
     private static int DriftTowards(int current, int target, int amount)
