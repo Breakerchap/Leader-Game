3e73f64f2580267c169207bfa29639d6f066680f
Remy E <remyscalendar@gmail.com>
2026-09-26 19:14:55 +1000

    Carry a war aim through declarations

--- Simulation/Orders/DeclareWarOrder.cs
+++ Simulation/Orders/DeclareWarOrder.cs
@@ -1,4 +1,5 @@
 using LeaderGame.Simulation.Countries;
+using LeaderGame.Simulation.Military;
 
 namespace LeaderGame.Simulation.Orders;
 
@@ -7,4 +8,7 @@ public sealed class DeclareWarOrder : Order
     public required Country SourceCountry { get; init; }
 
     public required Country TargetCountry { get; init; }
+
+    public WarAim Aim { get; init; } =
+        WarAim.Reparations;
 }
